e19d1acb9c1ffab333ae08298877eea3c4277a09
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-07-26 13:26:34 +0545

    Add project files.

---
+++ MSRDWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MSRDWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
